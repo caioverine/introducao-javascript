545f6d47019a6b9957026e5fc07785909d48aac4
caio.cvac <caio.cvac@dpf.gov.br>
2017-12-07 14:34:20 -0200

    Aula 04

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,8 @@
 var pacientes = document.querySelectorAll(".paciente");
+var titulo = document.querySelector("#titulo-form");
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+var form = document.querySelector("#form-adiciona");
+var tabela = document.querySelector("#tabela-pacientes");
 
 for (var i = 0; i < pacientes.length; i++) {
 
@@ -29,8 +33,37 @@ for (var i = 0; i < pacientes.length; i++) {
         paciente.classList.add("paciente-invalido");
     }
 
-  if (alturaEhValida && pesoEhValido) {
+    if (alturaEhValida && pesoEhValido) {
         var imc = peso / (altura * altura);
         tdImc.textContent = imc.toFixed(2);
     }
 }
+
+botaoAdicionar.addEventListener("click", function(event) {
+
+  event.preventDefault();
+  var nome = form.nome.value;
+  var peso = form.peso.value;
+  var altura = form.altura.value;
+  var gordura = form.gordura.value;
+
+  var pacienteTr = document.createElement("tr");
+
+  var nomeTd = document.createElement("td");
+  var pesoTd = document.createElement("td");
+  var alturaTd = document.createElement("td");
+  var gorduraTd = document.createElement("td");
+  var imcTd = document.createElement("td");
+
+  nomeTd.textContent = nome;
+  pesoTd.textContent = peso;
+  alturaTd.textContent = altura;
+  gorduraTd.textContent = gordura;
+
+  pacienteTr.appendChild(nomeTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(gorduraTd);
+
+  tabela.appendChild(pacienteTr);
+});
